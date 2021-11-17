51b4f28ff5f56c8ff00b09fe15bf176ec0bdd156
Shim MunSeong <Puterism.K@gmail.com>
2021-11-17 21:10:58 +0900

    feat: 각 건물별 path 지정

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,7 +1,26 @@
 export enum Building {
   R = 'R',
   K = 'K',
+  L = 'L',
+  J = 'J',
+  I = 'I',
+  G = 'G',
+  H = 'H',
   P = 'P',
+  Q = 'Q',
+  MH = 'MH',
+  F = 'F',
+  E = 'E',
+  U = 'U',
+  ABCD = 'ABCD',
+  DORM = 'DORM', // 제2기숙사,
+  T = 'T',
+  S = 'S',
+  Z = 'Z',
+  M = 'M',
+  PG = 'PG', // 운동장,
+  YM = 'YM', // 영원한 미소,
+  DD = 'DD', // 동땡
 }
 
 export interface Comment {
